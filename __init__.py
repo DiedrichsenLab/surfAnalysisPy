45afa43b94e9368a180f862522d7423d61a7d3fe
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2019-03-26 13:51:52 -0400

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# surfAnalysis package to run surface pipeline 
+# from <module name> import <function name>  
+from affineTransform import affineTransform
+from getGiftiAnatomicalStruct import getGiftiAnatomicalStruct

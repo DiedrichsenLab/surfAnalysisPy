b377022b1fde3b3f8faf4143f2b29a19ec467cee
STW <switt4@uwo.ca>
2019-06-18 12:50:11 -0400

    Cleaned up script
    Added documentation and fixed variable names to be more descriptive.

--- resliceFS2WB.py
+++ resliceFS2WB.py
@@ -3,7 +3,30 @@
 """
 Created on Wed Mar 20 14:10:56 2019
 
-@author: switt
+Resamples a registered subject surface from freesurfer average to the new
+symmetric fs_LR_32 surface, standard in workbench.  
+This allows things to happen exactly in atlas space - each vertex number
+corresponds exactly to a anatomical location. 
+For more information, see: 
+https://wiki.humanconnectome.org/download/attachments/63078513/Resampling-FreeSurfer-HCP_5_8.pdf
+
+INPUT: 
+   subjName: subject name
+   subjDir: freesurfer's SUBJECT_DIR (or location of freesurfer output)
+   outDir: Location to where new resampled files will be written (outDir/subjName)
+ VARARGIN: 
+   'hemisphere'        : left / right or both hemispheres 
+                       Default = [0,1]
+   'alignSurf':        : Shift the surface to correct for freesurfer convention?
+                       Default = [1,1,1] 
+   'surfFiles'         : Surface files to be resampled 
+                       Default = [".white",".pial",".inflated"]
+   'curvFiles'         : Curvature files to be resampled 
+                       Default = [".curv",".sulc",".area"]
+   'resolution'        : Resolution can be either set to '164k' or '32k'.
+                       Default =  "32k"
+
+@author: jdiedrichsen, switt
 """
 import os
 import re
@@ -31,30 +54,28 @@ def resliceFS2WB(subjName,subjDir,outDir,\
     hem = ["lh","rh"]
     Hem = ["L","R"]
     
-    currentDir = os.getcwd()
-    direct = os.path.join(os.getenv("FREESURFER_HOME"),"average","surf")
+    currentDirectory = os.getcwd()
+    freesurferAverageSurfaceDirectory = os.path.join(os.getenv("FREESURFER_HOME"),"average","surf")
     
     if not subjDir:
         subjDir = os.getenv("SUBJECTS_DIR")
     
-    # read freesurfer version
-    verFile = os.path.join(os.getenv("FREESURFER_HOME"),"build-stamp.txt") 
-    f = open(verFile, 'r')
-    verStr = f.readline()
+    # read in freesurfer version
+    freesurferVersionFile = os.path.join(os.getenv("FREESURFER_HOME"),"build-stamp.txt") 
+    f = open(freesurferVersionFile, 'r')
+    freesurferVersionString = f.readline()
     f.close()
-    verStr = verStr.replace('-v',' ')
-    verStr = re.split('[ -F]+', verStr)
-    fslVer = verStr[5]
+    freesurferVersionString = freesurferVersionString.replace('-v',' ')
+    freesurferVersionString = re.split('[ -F]+', freesurferVersionString)
+    freesurferVersion = freesurferVersionString[5]
     
-    # create new output directory
-    newDir = os.path.join(outDir,subjName)
-    if not newDir:
-        os.mkdir(newDir)
+    # create new output directory for subject
+    subjOutDir = os.path.join(outDir,subjName)
+    if not subjOutDir:
+        os.mkdir(subjOutDir)
      
-        #---------------------
-        #Transform surface files to standard mesh
-    numSurf = len(surfFiles)
-    numCurv = len(curvFiles)
+    numSurfFiles = len(surfFiles)
+    numCurvFiles = len(curvFiles)
     os.chdir(os.path.join(subjDir,subjName,"surf"))
     
     #---------------------
@@ -63,22 +84,23 @@ def resliceFS2WB(subjName,subjDir,outDir,\
     # the center of the 256x256x256 image.
     # Independent of the real zero point in the original image
     # So to find a transform of the
-    # Mvox2surf: Transform of voxels in 256x256 image to surface vertices
-    # Mvox2space: Transform of voxel to subject space
-    anaFile = os.path.join(subjDir,subjName,"mri","brain.mgz")
-    p = subprocess.run(["mri_info", anaFile, "--vox2ras-tkr"],\
+    # vox2surfTransformMatrix: Transform of voxels in 256x256 image to surface vertices
+    # vox2spaceTransformMatrix: Transform of voxel to subject space
+
+    anatFile = os.path.join(subjDir,subjName,"mri","brain.mgz")
+    mriInfoVox2RasTkrProcess = subprocess.run(["mri_info", anatFile, "--vox2ras-tkr"],\
                          stdout=subprocess.PIPE,stderr=subprocess.PIPE).\
                          stdout.decode('utf-8').split()
-    outp = np.array(list(map(float,p)))
-    Mvox2surf = outp.reshape(-1,4)
+    mriInfoVox2RasTkrProcessOutput = np.array(list(map(float,mriInfoVox2RasTkrProcess)))
+    vox2surfTransformMatrix = mriInfoVox2RasTkrProcessOutput.reshape(-1,4)
 
 
-    q = subprocess.run(["mri_info", anaFile, "--vox2ras"],\
+    mriInfoVox2RasProcess = subprocess.run(["mri_info", anatFile, "--vox2ras"],\
                        stdout=subprocess.PIPE,stderr=subprocess.PIPE).\
                        stdout.decode('utf-8').split()
-    outq = np.array(list(map(float,q)))
-    Mvox2space = outq.reshape(-1,4)
-    Msurf2space = np.matmul(Mvox2space,np.linalg.inv(Mvox2surf))
+    mriInfoVox2RasProcessOutput = np.array(list(map(float,mriInfoVox2RasProcess)))
+    vox2spaceTransformMatrix = mriInfoVox2RasProcessOutput.reshape(-1,4)
+    surf2spaceTransformMatrix = np.matmul(vox2spaceTransformMatrix,np.linalg.inv(vox2surfTransformMatrix))
     
     #---------------------
     #Transform the surfaces from the two hemispheres
@@ -89,16 +111,16 @@ def resliceFS2WB(subjName,subjDir,outDir,\
         subprocess.call(["mris_convert", ('.'.join((hem[h],"sphere.reg"))),regSphere])
         
     #-----------------------
-    #do all the surface files
-        for i in range(numSurf):
+    #Transform all the surface files
+        for i in range(numSurfFiles):
             #Set up file names
             fileName = '.'.join((hem[h],surfFiles[i],"surf.gii"))
             
             if len(subjName) == 0:
-                outName = os.path.join(newDir,('.'.join((Hem[h],surfFiles[i],\
+                surfGiftiFileName = os.path.join(subjOutDir,('.'.join((Hem[h],surfFiles[i],\
                                                          resolution,'surf.gii'))))
             else:
-                outName = os.path.join(newDir,('.'.join((subjName,Hem[h],\
+                surfGiftiFileName = os.path.join(subjOutDir,('.'.join((subjName,Hem[h],\
                                                          surfFiles[i],resolution,'surf.gii'))))
                 
             atlasName = os.path.join(atlasDir,"resample_fsaverage",\
@@ -111,28 +133,29 @@ def resliceFS2WB(subjName,subjDir,outDir,\
 
             subprocess.run(["wb_command", "-surface-resample",\
                              fileName,regSphere,atlasName,\
-                             "BARYCENTRIC",outName])
+                             "BARYCENTRIC",surfGiftiFileName])
             
-            A = nb.load(outName)
+            surfGifti = nb.load(surfGiftiFileName)
+            
             if (alignSurf[i]):
-                [A.darrays[0].coordsys.xform[:,0],A.darrays[0].coordsys.xform[:,1],\
-                 A.darrays[0].coordsys.xform[:,2]]=\
-                 affineTransform.affineTransform(A.darrays[0].\
-                 coordsys.xform[:,0],A.darrays[0].coordsys.xform[:,1],\
-                 A.darrays[0].coordsys.xform[:,2],Msurf2space)
+                [surfGifti.darrays[0].coordsys.xform[:,0],surfGifti.darrays[0].coordsys.xform[:,1],\
+                 surfGifti.darrays[0].coordsys.xform[:,2]]=\
+                 affineTransform.affineTransform(surfGifti.darrays[0].\
+                 coordsys.xform[:,0],surfGifti.darrays[0].coordsys.xform[:,1],\
+                 surfGifti.darrays[0].coordsys.xform[:,2],surf2spaceTransformMatrix)
                 
-            nb.save(A,outName)
+            nb.save(surfGifti,surfGiftiFileName)
             
     #-------------------------
-    #Do all the curvature files
-        for i in range(numCurv):
+    #Transform all the curvature files
+        for i in range(numCurvFiles):
             #Set up file names
             fileName = '.'.join((hem[h],curvFiles[i],"shape.gii"))
             if len(subjName) == 0:
-                outName = os.path.join(newDir,('.'.join((Hem[h],curvFiles[i],\
+                curvGiftiFileName = os.path.join(subjOutDir,('.'.join((Hem[h],curvFiles[i],\
                                                          resolution,"shape.gii"))))
             else:
-                outName = os.path.join(newDir,('.'.join((subjName,Hem[h],\
+                curvGiftiFileName = os.path.join(subjOutDir,('.'.join((subjName,Hem[h],\
                                                          curvFiles[i],resolution,"shape.gii"))))
             atlasName = os.path.join(atlasDir,"resample_fsaverage",\
                                      (''.join(("fs_LR-deformed_to-fsaverage.",\
@@ -141,7 +164,7 @@ def resliceFS2WB(subjName,subjDir,outDir,\
             subprocess.run(["mris_convert", "-c", ('.'.join((hem[h],curvFiles[i]))),\
                             ('.'.join((hem[h],surfFiles[0]))), fileName])
             subprocess.run(["wb_command", "-metric-resample",\
-                             fileName, regSphere, atlasName, "BARYCENTRIC", outName])           
+                             fileName, regSphere, atlasName, "BARYCENTRIC", curvGiftiFileName])           
             
             
             
